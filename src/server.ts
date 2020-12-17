25e31dbc88ff448d0d125969bcf4957d9d090f8b
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-12-17 19:00:07 -0300

    chore: config build

--- src/server.ts
+++ src/server.ts
@@ -17,5 +17,5 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Server started on port 3333!')
+  console.log('server is listening on 3333')
 });
