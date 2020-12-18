01180b21b7688c0e5502f24f2dbc2fa6ed65570e
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-12-18 10:45:49 -0300

    feat: save data from csv file

--- src/saveData.ts
+++ src/saveData.ts
@@ -1,8 +1,8 @@
 import csv from 'csv-parser'
 import fs from 'fs'
-import { ClinicModel } from './models/Clinic';
+import Clinic, { ClinicModel } from './models/Clinic';
 
-export const saveData = () => {
+export const parseData = () => {
   const filePath = `${__dirname}/data/data.csv` 
 
   let results: ClinicModel[] = [];
@@ -11,9 +11,13 @@ export const saveData = () => {
     .pipe(csv({
       mapHeaders: ({ header }) => renameHeaders(header)
     }))
-    .on('data', (data) => results.push(data))
+    .on('data', (data) => save(data))
   
   return results
+}
+
+export const save = async (clinic: ClinicModel) => {
+  await Clinic.create(clinic)
 }
 
 const renameHeaders = (header: string) => {
--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import routes from './routes'
+
 dotenv.config()
 
 const app = express()
 
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-)
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use(cors())
 app.use(express.json())
